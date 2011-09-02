9cee3174a7c042572db3e8ec3c0afb955fc9cd5c
Dmytrii Nagirniak <dnagir@gmail.com>
2011-09-03 00:25:41 +1000

    update dummy app

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,14 +1,13 @@
 require File.expand_path('../boot', __FILE__)
 
-#require 'rails/all'
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "active_resource/railtie"
-require "rails/test_unit/railtie"
-require 'sprockets/railtie'
+require 'rails/all'
 
-Bundler.require
-require "pakunok"
+if defined?(Bundler)
+  # If you precompile assets before deploying to production, use this line
+  Bundler.require *Rails.groups(:assets => %w(development test))
+  # If you want your assets lazily compiled in production, use this line
+  # Bundler.require(:default, :assets, Rails.env)
+end
 
 module Dummy
   class Application < Rails::Application
@@ -42,6 +41,8 @@ module Dummy
 
     # Enable the asset pipeline
     config.assets.enabled = true
+
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
   end
 end
-
